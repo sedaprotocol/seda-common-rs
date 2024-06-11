63d6467128abce3822e5761ef9a0f489299bd8a6
Mario Cao <mariocao@gmail.com>
2024-06-11 12:54:10 +0200

    fix(msgs): add deserialize to DataResult

--- src/msgs/data_requests/types.rs
+++ src/msgs/data_requests/types.rs
@@ -126,7 +126,7 @@ impl From<DataResult> for DR {
 
 /// Represents a resolved data result
 #[cfg_attr(feature = "cosmwasm", cw_serde)]
-#[cfg_attr(not(feature = "cosmwasm"), derive(Serialize))]
+#[cfg_attr(not(feature = "cosmwasm"), derive(Serialize, Deserialize))]
 #[cfg_attr(not(feature = "cosmwasm"), serde(rename_all = "snake_case"))]
 pub struct DataResult {
     // DR Result
