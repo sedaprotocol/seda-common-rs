f130c4d44c1c2be0b2a0734626247232566954c1
gluax <16431709+gluax@users.noreply.github.com>
2024-07-22 07:43:45 -0700

    refactor: dr query options

--- src/msgs/data_requests/query.rs
+++ src/msgs/data_requests/query.rs
@@ -5,7 +5,7 @@ use super::*;
 #[cfg_attr(not(feature = "cosmwasm"), derive(Serialize))]
 #[cfg_attr(not(feature = "cosmwasm"), serde(rename_all = "snake_case"))]
 pub enum QueryMsg {
-    #[cfg_attr(feature = "cosmwasm", returns(DataRequest))]
+    #[cfg_attr(feature = "cosmwasm", returns(Option<DataRequest>))]
     GetDataRequest { dr_id: String },
     #[cfg_attr(feature = "cosmwasm",  returns(Option<String>))]
     GetDataRequestCommitment { dr_id: String, public_key: String },
@@ -15,7 +15,7 @@ pub enum QueryMsg {
     GetDataRequestReveal { dr_id: String, public_key: String },
     #[cfg_attr(feature = "cosmwasm",  returns(HashMap<String, RevealBody>))]
     GetDataRequestReveals { dr_id: String },
-    #[cfg_attr(feature = "cosmwasm", returns(DataResult))]
+    #[cfg_attr(feature = "cosmwasm", returns(Option<DataResult>))]
     GetDataResult { dr_id: String },
     #[cfg_attr(feature = "cosmwasm",  returns(Vec<DataRequest>))]
     GetDataRequestsByStatus {
