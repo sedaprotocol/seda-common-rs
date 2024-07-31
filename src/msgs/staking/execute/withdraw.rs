d54b3eb2565904dc7b3cbb423b56235faad28c98
gluax <16431709+gluax@users.noreply.github.com>
2024-07-31 00:53:25 -0700

    chore: clean up cfg amount

--- src/msgs/staking/execute/withdraw.rs
+++ src/msgs/staking/execute/withdraw.rs
@@ -20,9 +20,6 @@ impl SignSelf for Execute {
     fn msg_hash(&self, chain_id: &str, contract_addr: &str, sequence: Self::Extra) -> Result<Hash> {
         Ok(hash([
             "withdraw".as_bytes(),
-            #[cfg(not(feature = "cosmwasm"))]
-            &self.amount.to_be_bytes(),
-            #[cfg(feature = "cosmwasm")]
             &self.amount.to_be_bytes(),
             chain_id.as_bytes(),
             contract_addr.as_bytes(),
