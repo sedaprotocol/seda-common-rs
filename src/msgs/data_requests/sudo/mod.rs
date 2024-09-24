b75bc51a1b410eb545ad90a53b00b76bbfad5247
hacheigriega <hykim.home@gmail.com>
2024-09-24 16:20:17 -0400

    fix: remove unused import

--- src/msgs/data_requests/sudo/mod.rs
+++ src/msgs/data_requests/sudo/mod.rs
@@ -7,7 +7,7 @@ pub mod post_results;
 #[cfg_attr(not(feature = "cosmwasm"), derive(serde::Serialize, Debug, PartialEq))]
 #[cfg_attr(not(feature = "cosmwasm"), serde(rename_all = "snake_case"))]
 pub struct PostResult {
-    pub dr_id:     String,
+    pub dr_id: String,
 }
 
 impl From<PostResult> for crate::msgs::SudoMsg {
