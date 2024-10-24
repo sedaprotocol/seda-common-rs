2dfc5df7301415440a33158a8c327cae282d6ce0
gluax <16431709+gluax@users.noreply.github.com>
2024-10-24 12:37:24 -0700

    feat: new protobuf files and match latest version of chain

--- crates/proto-common/src/gen/mod.rs
+++ crates/proto-common/src/gen/mod.rs
@@ -1,27 +1,37 @@
+#[cfg(feature = "batching")]
+#[path = "sedachain.batching.v1.rs"]
+#[rustfmt::skip]
+pub mod batching;
+
 #[cfg(feature = "data_proxy")]
 #[path = "sedachain.data_proxy.v1.rs"]
 #[rustfmt::skip]
-pub mod sedachain_data_proxy;
+pub mod data_proxy;
+
+#[cfg(feature = "pubkey")]
+#[path = "sedachain.pubkey.v1.rs"]
+#[rustfmt::skip]
+pub mod pubkey;
 
 #[cfg(feature = "randomness")]
 #[path = "sedachain.randomness.v1.rs"]
 #[rustfmt::skip]
-pub mod sedachain_randomness;
+pub mod randomness;
 
 #[cfg(feature = "staking")]
 #[path = "sedachain.staking.v1.rs"]
 #[rustfmt::skip]
-pub mod sedachain_staking;
+pub mod staking;
 
 #[cfg(feature = "vesting")]
 #[path = "sedachain.vesting.v1.rs"]
 #[rustfmt::skip]
-pub mod sedachain_vesting;
+pub mod vesting;
 
 #[cfg(feature = "wasm_storage")]
 #[path = "sedachain.wasm_storage.v1.rs"]
 #[rustfmt::skip]
-pub mod sedachain_wasm_storage;
+pub mod wasm_storage;
 
 // re-export prost
 #[cfg(feature = "cosmos")]
