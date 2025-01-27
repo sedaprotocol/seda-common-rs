fbdd5e88c0df9b48eaa20a75a26566b5a1b10cb9
gluax <16431709+gluax@users.noreply.github.com>
2025-01-27 11:44:51 -0800

    feat: pausable

--- crates/common/src/msgs/owner/execute/mod.rs
+++ crates/common/src/msgs/owner/execute/mod.rs
@@ -1,7 +1,9 @@
 pub mod accept_ownership;
 pub mod add_to_allowlist;
+pub mod pause;
 pub mod remove_from_allowlist;
 pub mod transfer_ownership;
+pub mod unpause;
 
 #[cfg_attr(feature = "cosmwasm", cosmwasm_schema::cw_serde)]
 #[cfg_attr(not(feature = "cosmwasm"), derive(serde::Serialize, Debug, PartialEq))]
@@ -13,6 +15,8 @@ pub enum ExecuteMsg {
     AddToAllowlist(add_to_allowlist::Execute),
     /// Remove a user from the allowlist.
     RemoveFromAllowlist(remove_from_allowlist::Execute),
+    Pause(pause::Execute),
+    Unpause(unpause::Execute),
 }
 
 impl From<ExecuteMsg> for crate::msgs::ExecuteMsg {
---
+++ crates/common/src/msgs/owner/execute/pause.rs
@@ -0,0 +1,10 @@
+#[cfg_attr(feature = "cosmwasm", cosmwasm_schema::cw_serde)]
+#[cfg_attr(not(feature = "cosmwasm"), derive(serde::Serialize, Debug, PartialEq))]
+#[cfg_attr(not(feature = "cosmwasm"), serde(rename_all = "snake_case"))]
+pub struct Execute {}
+
+impl From<Execute> for crate::msgs::ExecuteMsg {
+    fn from(value: Execute) -> Self {
+        super::ExecuteMsg::Pause(value).into()
+    }
+}
